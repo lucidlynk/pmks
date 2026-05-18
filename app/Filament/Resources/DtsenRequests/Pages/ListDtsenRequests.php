23c99bf81887d769af5d708b252081069b2f669c
iwan <buset.iwan@gmail.com>
2026-05-18 02:41:10 +0000

    fix: tambah tombol Create di ListDtsenRequests

--- app/Filament/Resources/DtsenRequests/Pages/ListDtsenRequests.php
+++ app/Filament/Resources/DtsenRequests/Pages/ListDtsenRequests.php
@@ -1,11 +1,16 @@
 <?php
-
 namespace App\Filament\Resources\DtsenRequests\Pages;
-
 use App\Filament\Resources\DtsenRequests\DtsenRequestResource;
+use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
-
 class ListDtsenRequests extends ListRecords
 {
     protected static string $resource = DtsenRequestResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            CreateAction::make(),
+        ];
+    }
 }
